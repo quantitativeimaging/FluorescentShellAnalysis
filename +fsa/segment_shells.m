80c955e759de7dc9c2bd07a01707a28582ab487f
James Manton <ajd.manton@googlemail.com>
2015-11-24 17:56:28 +0000

    Change segmentation to return a cell array, rather than concatenated array

--- +fsa/segment_shells.m
+++ +fsa/segment_shells.m
@@ -1,6 +1,6 @@
 function shell_images = segment_shells(image_data, centres, segment_half_size)
 
-shell_images = zeros(length(centres) * (2 * segment_half_size + 1), 2 * segment_half_size + 1);
+shell_images = cell(length(centres), 1);
 
 for i=1:length(centres)
 	row_centre = floor(centres(i, 2));
@@ -8,7 +8,7 @@ for i=1:length(centres)
 	col_centre = floor(centres(i, 1));
 	cols = (col_centre - segment_half_size):(col_centre + segment_half_size);
 	shell_image = image_data(rows, cols);
-	shell_images((1 + (i - 1) * size(shell_image, 1)) : (i * size(shell_image, 1)), :) = shell_image;
+	shell_images(i) = {shell_image};
 end
 
 end
