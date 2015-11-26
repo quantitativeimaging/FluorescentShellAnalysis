0a598274a63d6beaa6ec113d3aeb39c105250a19
James Manton <ajd.manton@googlemail.com>
2015-11-26 18:53:07 +0000

    Fix error in spherical cross section equation

--- +fsa/cross_section_sphere_thin.m
+++ +fsa/cross_section_sphere_thin.m
@@ -5,7 +5,7 @@ function I = cross_section_sphere_thin(x_centre, y_centre, radius, psf_sigma, he
 % 	x_centre  - x coordinate of shell centre.
 % 	y_centre  - y coordinate of shell centre.
 % 	radius    - Shell radius.
-% 	psf_sigma - Sigma of Gaussian point spread function.
+% 	psf_sigma - Standard deviation of Gaussian point spread function.
 % 	height    - Height of image intensity.
 % 	X         - Array of (x, y) coordinates.
 %
@@ -14,9 +14,10 @@ function I = cross_section_sphere_thin(x_centre, y_centre, radius, psf_sigma, he
 
 % Radial position
 r = sqrt((X(:, 1) - x_centre).^2 + (X(:, 2) - y_centre).^2);
+
 psf_variance = psf_sigma^2;
 
-I = height * (exp(-(r - radius).^2 / (2 * psf_sigma)) - exp(-(r + radius).^2 / (2 * psf_sigma))) ./ r;
+I = height * (exp(-(r - radius).^2 / (2 * psf_variance)) - exp(-(r + radius).^2 / (2 * psf_variance))) ./ r;
 
 % For r = 0, a singular point, use the limiting case
 I(r == 0) = (2 * radius * height / psf_variance) * exp(-(radius^2) / (2 * psf_variance));
